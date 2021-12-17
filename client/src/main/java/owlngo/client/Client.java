b4c8dfe449c0acd5ec44459fe94fcd8863550eca
A. Schenker <a.schenker@cip.ifi.lmu.de>
2021-12-17 23:38:50 +0100

    Test: Reset project structure to multiple modules
    Since the structure didn't seem to solve the problem causing the pipeline to fail the build, I reverted it back to its original state.
    
    As the application modules require a main class, I implemented a class which just prints a text
    message as the run task is started.
    
    To actually start the modules using gradle I had to set the main class in the build.gradle.

---
+++ client/src/main/java/owlngo/client/Client.java
@@ -0,0 +1,9 @@
+package owlngo.client;
+
+public class Client {
+
+  public static void main(String[] args) {
+    System.out.println("Hello client!");
+  }
+
+}
---
+++ server/src/main/java/owlngo/server/Server.java
@@ -0,0 +1,9 @@
+package owlngo.server;
+
+public class Server {
+
+  public static void main(String[] args) {
+    System.out.println("Hello server!");
+  }
+
+}
