176863500069e6caf3ef240c34b8afe783caac72
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-03 11:18:15 +0100

    Implemented interface ObjectInGame.java
    This interface is used to generalize the behaviour of ingame objects.
    
    Co-Authored-By: J. Zitzlsperger <zitzlsperger@cip.ifi.lmu.de>

--- game/src/main/java/owlngo/game/level/objects/ObjectInGame.java
+++ game/src/main/java/owlngo/game/level/objects/ObjectInGame.java
@@ -1,4 +1,48 @@
 package owlngo.game.level.objects;
 
+import owlngo.game.level.Coordinate;
+import owlngo.game.level.Level;
+
+/** Represents the general objects in {@link Level} */
 public interface ObjectInGame {
+
+  enum ObjectType {
+    NONE,
+    PLAYER,
+    START,
+    FINISH,
+    GROUND
+  }
+
+  /**
+   * Returns the object type of the ingame object.
+   *
+   * @return the object type
+   */
+  ObjectType getType();
+
+  /**
+   * Returns the current position of the ingame object.
+   *
+   * @return the coordinates of the object
+   */
+  Coordinate getCoordinate();
+
+  /**
+   * Checks if this ingame object has valid attributes.
+   *
+   * @return <code>true</code> if object attributes meet the requirements, otherwise <code>false
+   *     </code>
+   */
+  boolean isValid();
+
+  /**
+   * Checks if this ingame object is equal to the global dummy object <code>ObjectType.NONE</code>.
+   *
+   * @return <code>true</code> if object attributes meet the requirements, otherwise <code>false
+   *     </code>
+   */
+  boolean isNone();
+
+  String toString();
 }
