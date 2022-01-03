3bd68aa389f3ce4e68a99b11aa99bbcf21977ca7
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-03 12:43:05 +0100

    Modified Editor
    IntelliJ made some suggestions to improve the code.
    Also, the JavaDoc for the class has been added.

--- gui/src/main/java/owlngo/editor/Editor.java
+++ gui/src/main/java/owlngo/editor/Editor.java
@@ -1,7 +1,6 @@
 package owlngo.editor;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,16 +9,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-// This class opens the Playfield-Playfield by the start method that is required by fxml
-// This class also sets a handle for key events on the scene.
-
+/** Starter class for the editor window. */
 public class Editor extends Application {
   public static void main(String[] args) {
     launch(args);
   }
 
   @Override
-  // public void start(Stage primaryStage) throws Exception {
   public void start(Stage primaryStage) {
     StoreLastKey.setSpaceAsDefault();
 
@@ -34,21 +30,15 @@ public class Editor extends Application {
       Scene scene = new Scene(root, 800, 600);
       // Set up keyHandler
       scene.setOnKeyPressed(
-          new EventHandler<>() {
-            @Override
-            public void handle(javafx.scene.input.KeyEvent event) {
-              KeyCode keyCode = event.getCode();
-              // System.out.println("Tastendruck:"+ keyCode);
-              StoreLastKey.setLastKeyPressed(event.getCode()); // StoreKey in order to get
-              // the last press key asynchonous during mouse click events
-            }
+          event -> {
+            KeyCode keyCode = event.getCode();
+            StoreLastKey.setLastKeyPressed(keyCode); // StoreKey in order to get
+            // the last press key asynchonous during mouse click events
           });
 
       stage.setScene(scene);
-      // stage.setScene(new Scene(root, 800, 600));
       stage.setResizable(true);
       stage.show();
-      //       ((Node) event.getSource()).getScene().getWindow().hide();  // hide StartWindow
     } catch (IOException e) {
       e.getCause().getCause();
       System.exit(0);
