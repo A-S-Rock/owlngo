0ccdd7bdc0953a5e0565bc697d5fe6ad832025ee
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-03 12:14:26 +0100

    Added JavaDocs for some classes
    To add the source for foreign code, we adjusted some JavaDocs.
    Also, we added the JavaDoc for the MoveType enum.
    
    Co-Authored-By: J. Zitzlsperger <zitzlsperger@cip.ifi.lmu.de>

--- game/src/main/java/owlngo/game/level/Move.java
+++ game/src/main/java/owlngo/game/level/Move.java
@@ -1,47 +1,54 @@
 package owlngo.game.level;
 
-import owlngo.game.level.objects.ObjectInGame;
+import owlngo.game.OwlnGo;
+import owlngo.game.level.objects.Player;
 
+/**
+ * This class represents the move of a {@link Player} on a {@link Level} of an {@link OwlnGo} game.
+ * The source code from this class has been adjusted from the Task 4 code.
+ */
 public final class Move {
 
-    /** Represents the types of moves. */
-    public enum MoveType { //TODO: Add extended JavaDocs for each move type.
-        FORWARD,
-        BACKWARD,
-        JUMP
-    }
+  /**
+   * Represents the types of moves. A {@link Player} can move in the <code>LEFT</code> or <code>
+   * RIGHT</code> direction and can also <code>JUMP</code>.
+   */
+  public enum MoveType { // TODO: Add extended JavaDocs for each move type.
+    LEFT,
+    RIGHT,
+    JUMP
+  }
 
-    final MoveType moveType;
-    final Coordinate newCoordinate;
+  final MoveType moveType;
+  final Coordinate newCoordinate;
 
-    /** Constructs a new Move instance that moves the character forward. */
-    public static Move newForwardMove(Coordinate coordinate) {
-        return new Move(MoveType.FORWARD, coordinate); // Task4, ChessPiece
-    }
+  /** Constructs a new Move instance that moves the character forward. */
+  public static Move newForwardMove(Coordinate coordinate) {
+    return new Move(MoveType.LEFT, coordinate); // Task4, ChessPiece
+  }
 
-    /** Constructs a new Move instance that moves the character backward. */
-    public static Move newBackwardMove(Coordinate coordinate) {
-        return new Move(MoveType.BACKWARD, coordinate); // Task4, ChessPiece
-    }
+  /** Constructs a new Move instance that moves the character backward. */
+  public static Move newBackwardMove(Coordinate coordinate) {
+    return new Move(MoveType.RIGHT, coordinate); // Task4, ChessPiece
+  }
 
-    /** Constructs a new Move instance that lets the character jump. */
-    public static Move newJumpMove(Coordinate coordinate) {
-        return new Move(MoveType.JUMP, coordinate); // Task4, ChessPiece
-    }
+  /** Constructs a new Move instance that lets the character jump. */
+  public static Move newJumpMove(Coordinate coordinate) {
+    return new Move(MoveType.JUMP, coordinate); // Task4, ChessPiece
+  }
 
-    private Move(MoveType moveType, Coordinate newCoordinate) {
-        this.moveType = moveType;
-        this.newCoordinate = newCoordinate;
-    }
+  private Move(MoveType moveType, Coordinate newCoordinate) {
+    this.moveType = moveType;
+    this.newCoordinate = newCoordinate;
+  }
 
-    /** Gets the type of the move. */
-    public MoveType getMoveType() {
-        return moveType;
-    }
+  /** Gets the type of the move. */
+  public MoveType getMoveType() {
+    return moveType;
+  }
 
-    /** Gets the new coordinate of the piece after applying the move. */
-    public Coordinate getNewCoordinate() {
-        return newCoordinate;
-    }
-
+  /** Gets the new coordinate of the piece after applying the move. */
+  public Coordinate getNewCoordinate() {
+    return newCoordinate;
+  }
 }
