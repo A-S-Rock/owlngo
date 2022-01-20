14ad8728e3c59eecfc5ed3bce18889454557d239
Roman <loeserr@cip.ifi.lmu.de>
2022-01-20 23:57:55 +0100

    Passed Checks

--- gui/src/main/java/owlngo/gui/controller/GameViewScreenController.java
+++ gui/src/main/java/owlngo/gui/controller/GameViewScreenController.java
@@ -1,17 +1,12 @@
 package owlngo.gui.controller;
 
-import java.io.IOException;
 import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 /** Contoller class for GameViewScreen.fxml. */
 public class GameViewScreenController {
