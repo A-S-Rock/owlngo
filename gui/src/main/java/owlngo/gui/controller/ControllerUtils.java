79638c0602230ad7d547db59451a17ea9db53faa
Roman <loeserr@cip.ifi.lmu.de>
2022-01-22 12:03:05 +0100

    Outsourced logo-themed rectangle generation

--- gui/src/main/java/owlngo/gui/controller/ControllerUtils.java
+++ gui/src/main/java/owlngo/gui/controller/ControllerUtils.java
@@ -2,7 +2,6 @@ package owlngo.gui.controller;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -22,6 +21,15 @@ public class ControllerUtils {
 
   private static final int LOGO_WIDTH = 300;
   private static final int LOGO_HEIGHT = LOGO_WIDTH;
+
+  private static final URL STANDARD_LOGO =
+      ControllerUtils.class.getResource("/images/logo_animated.gif");
+  private static final URL LOSE_LOGO =
+      ControllerUtils.class.getResource("/images/logo_animated.gif");
+  private static final URL GIVEUP_LOGO =
+      ControllerUtils.class.getResource("/images/logo_animated.gif");
+  private static final URL WIN_LOGO =
+      ControllerUtils.class.getResource("/images/logo_animated.gif");
 
   /**
    * Creates a new scene with a fxml-window.
@@ -43,21 +51,34 @@ public class ControllerUtils {
     }
   }
 
-  public Rectangle createLogoPane(String logoType) {
+  /**
+   * Creates a image-filled rectangle used as a logo depending on the game status.
+   *
+   * @param logoType is a string to diffentiate between win, lose, giveup and standard
+   * @return a properly styled rectangle
+   */
+  public static Rectangle createLogoPane(String logoType) {
     Image logoImage = null;
-    URL logoURL = null;
+    URL logoUrl;
     switch (logoType) {
+      case "standard":
+        logoUrl = STANDARD_LOGO;
+        break;
       case "win":
-        logoURL = getClass().getResource("/images/logo_animated.gif");
+        logoUrl = WIN_LOGO;
+        break;
       case "lose":
-        logoURL = getClass().getResource("/images/logo_animated.gif");
+        logoUrl = LOSE_LOGO;
+        break;
       case "giveup":
-        logoURL = getClass().getResource("/images/logo_animated.gif");
+        logoUrl = GIVEUP_LOGO;
+        break;
       default:
-        logoURL = getClass().getResource("/images/logo_animated.gif");
+        logoUrl = STANDARD_LOGO;
+        break;
     }
     try {
-      logoImage = new Image(Objects.requireNonNull(logoURL).toString());
+      logoImage = new Image(logoUrl.toString());
     } catch (IllegalArgumentException e) {
       System.err.println("Image not found.");
     }
