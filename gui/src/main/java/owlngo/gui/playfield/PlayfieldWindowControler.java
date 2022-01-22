180aace794c18e2d02f82daf05419ee134f0ca05
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-22 01:56:04 +0100

    Some more refactoring and updates to adhere code style

--- gui/src/main/java/owlngo/gui/playfield/PlayfieldWindowControler.java
+++ gui/src/main/java/owlngo/gui/playfield/PlayfieldWindowControler.java
@@ -2,57 +2,34 @@ package owlngo.gui.playfield;
 
 import static owlngo.gui.data.MethodsForElement.OBJECT_TYPE_ELEMENT_IN_PLAYFIELD_MAP;
 
-import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.util.Duration;
 import owlngo.game.OwlnGo;
 import owlngo.game.level.Coordinate;
 import owlngo.game.level.objects.ObjectInGame;
 import owlngo.gui.data.ElementsInPlayfield;
 import owlngo.gui.data.MethodsForElement;
 
-// This class handles all actions on the window EditorWindow.fxml
-// It also set the enum ElementInPlayfield for each pane that is used
-// in the gritPane of the window.
-
 /**
- * The class handles all actions on the window PlayfieldWindow.fxml It displays the element in
- * ElementsInPlayfield as panes in a gridpane. The background is set according to the elements. A
- * key handler is setup that allows to move the owl with 8,4,2,6.
+ * Handles all actions on the playfield window. Obsolete because one already exists. TODO: Delete
+ * and refactor classes if needed.
  */
 public class PlayfieldWindowControler {
-  // 6.1. 14.20
-  // Constant values for window
-  // static final int numberOfPanesInRowAnColumn = 30;
-  private static final Pane[][] pane = new Pane[MethodsForElement.SIZE][MethodsForElement.SIZE];
 
-  // private static final OwlnGo game = new OwlnGo(MethodsForElement.SIZE, MethodsForElement.SIZE);
-  private static final OwlnGo game = new OwlnGo(ElementsInPlayfield.getLevel());
-
-  // private static Coordinate oldCoordinate;
+  private static final Pane[][] PANE = new Pane[MethodsForElement.SIZE][MethodsForElement.SIZE];
+  private static final OwlnGo GAME = new OwlnGo(ElementsInPlayfield.getLevel());
 
   @FXML GridPane gridPanePlayfieldWindow;
-  // Name must be as fx:ID in gridPane in PlayfieldToControlerFirstVersion.fxml
-  // fx:id="gridPaneChessBoard"
-
-  @FXML Label displayToUser;
-  // Name must be as ID:ID  in Text in PlayfieldToControlerFirstVersion.fxml
 
   @FXML
   private void initialize() {
-    System.out.println("PlayfieldWindowControler");
-    // setChangeListner();
     getElementsOfPlayfieldFromGame();
     initializePanes();
     setPanesOnPlayfield();
-
-    // OwlnGo game= new OwlnGo();
   }
 
   private static void getElementsOfPlayfieldFromGame() {
@@ -60,11 +37,9 @@ public class PlayfieldWindowControler {
       for (int rowIndex = 0; rowIndex < MethodsForElement.SIZE; rowIndex++) {
         Coordinate coordinate = Coordinate.of(rowIndex, columnIndex);
         ObjectInGame.ObjectType objectType =
-            game.getGameState().getLevel().getObjectInGameAt(coordinate).getType();
-        // System.out.print(objectType+" ");
+            GAME.getGameState().getLevel().getObjectInGameAt(coordinate).getType();
         ElementsInPlayfield.ElementInPlayfield elementsInPlayfield;
         elementsInPlayfield = OBJECT_TYPE_ELEMENT_IN_PLAYFIELD_MAP.get(objectType);
-        // System.out.print(elementsInPlayfield+" ");
         ElementsInPlayfield.setElementTo(elementsInPlayfield, rowIndex, columnIndex);
       }
     }
@@ -75,7 +50,6 @@ public class PlayfieldWindowControler {
    * set in ElementsInPlayfield.
    */
   public static void changeAllPanesDependingOnElementsInPlayfied() {
-    System.out.println(("changeAllPanesDependingOnElementsInPlayfied"));
     for (int columnIndex = 0; columnIndex < MethodsForElement.SIZE; columnIndex++) {
       for (int rowIndex = 0; rowIndex < MethodsForElement.SIZE; rowIndex++) {
 
@@ -84,14 +58,12 @@ public class PlayfieldWindowControler {
             MethodsForElement.getBackgroundFill(
                 ElementsInPlayfield.getElement(rowIndex, columnIndex));
         Background background = new Background(backgroundFill);
-        pane[rowIndex][columnIndex].setBackground(background);
+        PANE[rowIndex][columnIndex].setBackground(background);
       }
     }
   }
 
-  // Set Background of pane depending on the content of elementsInPlayfield
   private static Pane setBackgroundOfPaneDependingOnContent(int row, int column) {
-    // System.out.println("setBackgroundOfPaneDependingOnContent");
     Pane pane = new Pane();
     BackgroundFill backgroundFill;
 
@@ -102,38 +74,33 @@ public class PlayfieldWindowControler {
     return pane;
   }
 
-  /* Initializes the Gamefield with panes */
   private void initializePanes() {
-    System.out.println("initializePanes");
     for (int columnIndex = 0; columnIndex < MethodsForElement.SIZE; columnIndex++) {
       for (int rowIndex = 0; rowIndex < MethodsForElement.SIZE; rowIndex++) {
-        pane[rowIndex][columnIndex] = new Pane();
+        PANE[rowIndex][columnIndex] = new Pane();
       }
     }
   }
 
   private void setPanesOnPlayfield() {
-    System.out.println("setPanesOnBoard");
-    // Pane[][] pane = new Pane[numberOfPanesInRowAnColumn][numberOfPanesInRowAnColumn];
     for (int columnIndex = 0; columnIndex < MethodsForElement.SIZE; columnIndex++) {
       for (int rowIndex = 0; rowIndex < MethodsForElement.SIZE; rowIndex++) {
         // Set Background of pane depending on the content of elementsInPlayfield
 
-        pane[rowIndex][columnIndex] = setBackgroundOfPaneDependingOnContent(rowIndex, columnIndex);
-        gridPanePlayfieldWindow.add(pane[rowIndex][columnIndex], columnIndex, rowIndex);
+        PANE[rowIndex][columnIndex] = setBackgroundOfPaneDependingOnContent(rowIndex, columnIndex);
+        gridPanePlayfieldWindow.add(PANE[rowIndex][columnIndex], columnIndex, rowIndex);
       }
     }
   }
 
   @FXML
   void restartGame() {
-
-    rotate360();
+    // TODO: Actually start a new game.
   }
 
   /**
-   * Depending on keyboard entries on the Num Keybord of the letters w, a, d an action for the game
-   * is started.
+   * Depending on keyboard entries on the Num Keybord of the letters WASD an action for the game is
+   * started.
    *
    * @param keyCode keyboard entry
    */
@@ -141,33 +108,17 @@ public class PlayfieldWindowControler {
     if (keyCode == KeyCode.NUMPAD8
         || keyCode == KeyCode.getKeyCode("w")
         || keyCode == KeyCode.getKeyCode("W")) {
-      game.moveJump(false);
+      GAME.moveJump(false);
     } else if ((keyCode == KeyCode.NUMPAD6)
         || (keyCode == KeyCode.getKeyCode("d"))
         || (keyCode == KeyCode.getKeyCode("D"))) {
-      game.moveRight();
+      GAME.moveRight();
     } else if (keyCode == KeyCode.NUMPAD4
         || keyCode == KeyCode.getKeyCode("a")
         || keyCode == KeyCode.getKeyCode("A")) {
-      game.moveLeft();
+      GAME.moveLeft();
     }
     getElementsOfPlayfieldFromGame();
     changeAllPanesDependingOnElementsInPlayfied();
-  }
-
-  void rotate360() {
-    RotateTransition rt = new RotateTransition(Duration.millis(1000), gridPanePlayfieldWindow);
-    rt.setByAngle(360);
-    rt.setCycleCount(1);
-    rt.setAutoReverse(false);
-    rt.play();
-  }
-
-  void rotate() {
-    RotateTransition rt = new RotateTransition(Duration.millis(1000), gridPanePlayfieldWindow);
-    rt.setByAngle(180);
-    rt.setCycleCount(1);
-    rt.setAutoReverse(true);
-    rt.play();
   }
 }
