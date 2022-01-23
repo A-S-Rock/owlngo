0e48996f55ace8feb7be1ed6f6a5c2bcab1640c7
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-23 13:11:14 +0100

    Added level savetype.

---
+++ communication/src/main/java/owlngo/communication/savefiles/LevelSavefile.java
@@ -0,0 +1,30 @@
+package owlngo.communication.savefiles;
+
+import owlngo.game.level.Level;
+
+public final class LevelSavefile implements Savefile {
+  @SuppressWarnings("unused")
+  private static final String savefileType = "LevelSavefile";
+
+  private final String levelName;
+  private final String author;
+  private final Level level;
+
+  public LevelSavefile(String levelName, String author, Level level) {
+    this.levelName = levelName;
+    this.author = author;
+    this.level = level;
+  }
+
+  public String getLevelName() {
+    return levelName;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public Level getLevel() {
+    return level;
+  }
+}
