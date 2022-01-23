c66361c33f3544bc602982df2f851b231a1be010
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-23 14:14:30 +0100

    Added test class in communication for JSON tests.
    This should be temporary and be replaced by JUnit tests.
    Also, a test tasks is added to gradle because this module is not
    an application.

---
+++ communication/src/main/java/owlngo/communication/TestMain.java
@@ -0,0 +1,9 @@
+package owlngo.communication;
+
+/** Mock tests for fast testing. Later on, JUnit should be used for this. */
+public class TestMain {
+
+  public static void main(String[] args) {
+    System.out.println("This works!");
+  }
+}
