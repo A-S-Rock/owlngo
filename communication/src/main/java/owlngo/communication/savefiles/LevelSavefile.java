eea5be5b3ca51aa522eb7c5d7ab0a06e331ba2bd
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-01-23 13:37:40 +0100

    CheckStyle, SpotBugs and Spotless checks

--- communication/src/main/java/owlngo/communication/savefiles/LevelSavefile.java
+++ communication/src/main/java/owlngo/communication/savefiles/LevelSavefile.java
@@ -2,6 +2,7 @@ package owlngo.communication.savefiles;
 
 import owlngo.game.level.Level;
 
+/** Used for the persistent saving of Level files including some stats. */
 public final class LevelSavefile implements Savefile {
   @SuppressWarnings("unused")
   private static final String savefileType = "LevelSavefile";
@@ -10,6 +11,13 @@ public final class LevelSavefile implements Savefile {
   private final String author;
   private final Level level;
 
+  /**
+   * Creates a new level savefile.
+   *
+   * @param levelName the level name given by the user
+   * @param author the creator player's name
+   * @param level the level layout
+   */
   public LevelSavefile(String levelName, String author, Level level) {
     this.levelName = levelName;
     this.author = author;
