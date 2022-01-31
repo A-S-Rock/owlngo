9246fd96ffbfe4a0b55039e972b2119a8a5b4329
Roman <loeserr@cip.ifi.lmu.de>
2022-01-31 12:45:04 +0100

    Added FoodView Class

---
+++ gui/src/main/java/owlngo/gui/playfield/view/FoodView.java
@@ -0,0 +1,30 @@
+package owlngo.gui.playfield.view;
+
+import java.util.Objects;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+
+/** Class adds a soil-themed rectangle as a ground/soil tile. */
+public class FoodView extends StackPane {
+
+  private Image foodImage;
+
+  /** Constructor loads a png-image and makes a rectangle filled with this image. */
+  public FoodView() {
+    try {
+      foodImage =
+          new Image(
+              Objects.requireNonNull(getClass().getResource("/images/food_animated.gif"))
+                  .toString());
+    } catch (IllegalArgumentException e) {
+      System.err.println("Image not found.");
+    }
+    Rectangle groundElement =
+        new Rectangle(GameView.TILE_SIZE, GameView.TILE_SIZE, Color.TRANSPARENT);
+    groundElement.setFill(new ImagePattern(foodImage));
+    getChildren().add(groundElement);
+  }
+}
