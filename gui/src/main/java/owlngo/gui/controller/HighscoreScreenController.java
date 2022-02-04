56512fc2238773c4e39165b367071429ef06d76c
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-02-04 23:38:15 +0100

    Updated Highscore view

--- gui/src/main/java/owlngo/gui/controller/HighscoreScreenController.java
+++ gui/src/main/java/owlngo/gui/controller/HighscoreScreenController.java
@@ -1,15 +1,30 @@
 package owlngo.gui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import owlngo.gui.data.LevelStatsRecord;
 
 /** Contoller class for HighscoreScreen.fxml. */
 public class HighscoreScreenController {
 
+  @FXML AnchorPane root;
   @FXML Button backToWelcomeScreenButton;
+  @FXML TableView<LevelStatsRecord> tableView;
+  @FXML TableColumn<LevelStatsRecord, String> levelNameColumn;
+  @FXML TableColumn<LevelStatsRecord, Integer> triesColumn;
+  @FXML TableColumn<LevelStatsRecord, Integer> completionsColumn;
+  @FXML TableColumn<LevelStatsRecord, String> bestTimeColumn;
+  @FXML TableColumn<LevelStatsRecord, String> byUserColumn;
+
+  private final List<LevelStatsRecord> levelStatsRecords = new ArrayList<>();
 
   @FXML
   void initialize() {
