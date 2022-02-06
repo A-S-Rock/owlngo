ce76db9f5ce2f48304cc47fbd61ec5beeda856c8
A. Schenker <a.schenker@cip.ifi.lmu.de>
2022-02-06 06:04:35 +0100

    Added music to Give up screen.

--- gui/src/main/java/owlngo/gui/controller/GameGivenUpScreenController.java
+++ gui/src/main/java/owlngo/gui/controller/GameGivenUpScreenController.java
@@ -1,18 +1,30 @@
 package owlngo.gui.controller;
 
+import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /** Contoller class for GameGivenUpScreen.fxml. */
 public class GameGivenUpScreenController {
+  private final MediaPlayer mediaPlayer;
   @FXML Button retryButton;
   @FXML Button backToWelcomeScreenButton;
   @FXML Button exitGameButton;
   @FXML Pane imagePane;
+
+  /** Adds music to the controller. */
+  public GameGivenUpScreenController() {
+    final Media media =
+        new Media(Objects.requireNonNull(getClass().getResource("/music/RICKROLL.m4a")).toString());
+    mediaPlayer = new MediaPlayer(media);
+    mediaPlayer.play();
+  }
 
   @FXML
   void initialize() {
@@ -20,6 +32,7 @@ public class GameGivenUpScreenController {
         new EventHandler<>() {
           @Override
           public void handle(ActionEvent event) {
+            mediaPlayer.stop();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GameViewScreen.fxml"));
             ControllerUtils.createScene(event, fxmlLoader);
           }
@@ -29,6 +42,7 @@ public class GameGivenUpScreenController {
         new EventHandler<>() {
           @Override
           public void handle(ActionEvent event) {
+            mediaPlayer.stop();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/WelcomeScreen.fxml"));
             ControllerUtils.createScene(event, fxmlLoader);
           }
--- gui/src/main/java/owlngo/gui/controller/GameOverScreenController.java
+++ gui/src/main/java/owlngo/gui/controller/GameOverScreenController.java
@@ -1,31 +1,19 @@
 package owlngo.gui.controller;
 
-import java.util.Objects;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 /** Contoller class for GameOverScreen.fxml. */
 public class GameOverScreenController {
-
-  private final MediaPlayer mediaPlayer;
 
   @FXML Button retryButton;
   @FXML Button backToWelcomeScreenButton;
   @FXML Button exitGameButton;
   @FXML Pane imagePane;
-
-  public GameOverScreenController() {
-    final Media media = new Media(
-        Objects.requireNonNull(getClass().getResource("music/RICKROLL.m4a")).toString());
-    mediaPlayer = new MediaPlayer(media);
-    mediaPlayer.play();
-  }
 
   @FXML
   void initialize() {
@@ -33,7 +21,6 @@ public class GameOverScreenController {
         new EventHandler<>() {
           @Override
           public void handle(ActionEvent event) {
-            mediaPlayer.stop();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GameViewScreen.fxml"));
             ControllerUtils.createScene(event, fxmlLoader);
           }
@@ -43,7 +30,6 @@ public class GameOverScreenController {
         new EventHandler<>() {
           @Override
           public void handle(ActionEvent event) {
-            mediaPlayer.stop();
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/WelcomeScreen.fxml"));
             ControllerUtils.createScene(event, fxmlLoader);
           }
